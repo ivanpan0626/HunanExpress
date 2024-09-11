c9409496feeb058780501ea8b55857800442cbd8
ivanpan0626 <151955212+ivanpan0626@users.noreply.github.com>
2024-09-11 01:28:41 -0400

    init layout for nav and hero section

---
+++ hunanexpress/src/app/page.js
@@ -0,0 +1,13 @@
+import Hero from "@/components/layout/Hero";
+import Header from "@/components/layout/Header"
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <Header></Header>
+      <Hero></Hero>
+    </>
+  );
+}
